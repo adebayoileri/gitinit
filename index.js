e4b72c79823b43726680abcef1fcf5367303fc67
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-03-30 13:51:02 +0100

    get github creadentials with inquirer

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const files = require('./lib/files');
+const inquirer  = require('./lib/inquirer');
 
+// if (files.directoryExists('.git')) {
+//   console.log(chalk.red('Already a Git repository!'));
+//   process.exit();
+// }
 
-console.log(
-    chalk.yellow(
-      figlet.textSync('Adebayo Ilerioluwa', { horizontalLayout: 'full' })
-    )
-  );
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials();
+  console.log(credentials);
+};
+
+run();
+// console.log(
+//     chalk.yellow(
+//       figlet.textSync('Adebayo Ilerioluwa', { horizontalLayout: 'full' })
+//     )
+//   );
